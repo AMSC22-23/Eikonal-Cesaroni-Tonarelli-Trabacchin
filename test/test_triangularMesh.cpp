744f5df71ef5eb4a9e4d909101f43b94514f4250
Sabrina Cesaroni <sabrina.cesaroni@mail.polimi.it>
2023-11-23 17:54:57 +0100

    prova include path

--- test/test_triangularMesh.cpp
+++ test/test_triangularMesh.cpp
@@ -1,10 +1,9 @@
 //
 // Created by sabri on 23/11/2023.
 //
-#include "TriangularMesh.h"
 #include <string>
 #include <iostream>
-
+#include "../src/TriangularMesh.h"
 int main(){
     std::string fileName = "triangular_mesh_test.vtx";
     TriangularMesh<2> mesh (fileName);
