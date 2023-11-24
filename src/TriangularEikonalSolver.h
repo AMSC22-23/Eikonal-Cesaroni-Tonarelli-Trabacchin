bd4212d1ca6c6122d2e8311573ae2065e3708491
Tommaso Trabacchin <tommaso.trabacchin@mail.polimi.it>
2023-11-24 10:28:39 +0100

    refactored the project

---
+++ src/TriangularEikonalSolver.h
@@ -0,0 +1,16 @@
+//
+// Created by tomma on 24/11/2023.
+//
+
+#ifndef EIKONAL_CESARONI_TONARELLI_TRABACCHIN_TRIANGULAREIKONALSOLVER_H
+#define EIKONAL_CESARONI_TONARELLI_TRABACCHIN_TRIANGULAREIKONALSOLVER_H
+
+#include "TriangularMesh.h"
+
+template<int D>
+class TriangularEikonalSolver {
+private:
+    TriangularMesh<D> mesh;
+
+};
+#endif //EIKONAL_CESARONI_TONARELLI_TRABACCHIN_TRIANGULAREIKONALSOLVER_H
